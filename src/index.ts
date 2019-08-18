851698641fab4437473ac92b67f2cb7f9314f670
Tatsuya Maki <t28oogle@gmail.com>
2019-08-18 13:52:09 +0900

    Setup development environment for the TypeScript (#1)

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import {APIGatewayEvent, Context, Callback, Handler} from 'aws-lambda';
+
+export const handler: Handler = async (event: APIGatewayEvent, context: Context, callback: Callback) => {
+  context.succeed({
+    statusCode: 200,
+    body: JSON.stringify(event),
+  });
+};
